e9a29cd99270e96c2f3622b9f7ba40e368345c4a
gibgibbb <diazkristian321@gmail.com>
2024-10-05 20:55:09 +0800

    changes to the token accessing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,18 +8,24 @@ const loggingMiddleware = (req, res, next) => {
 };
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
-  }
-
-  jwt.verify(token, SECRET_KEY, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+    console.log('Auth Header:', req.headers['authorization']);
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      return res.status(401).json({ message: 'No token provided' });
     }
-    req.userId = decoded.id;
-    next();
-  });
-};
+  
+    const token = authHeader.split(' ')[1];
+    console.log('Extracted Token:', token);
+  
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err) {
+        console.log('Token Verification Error:', err);
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      console.log('Decoded Token:', decoded);
+      req.userId = decoded.id;
+      next();
+    });
+  };
 
 module.exports = { loggingMiddleware, authMiddleware };
