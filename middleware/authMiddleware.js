d646f9bf1818ee4a04380c235a09b1f924a06e77
gibgibbb <diazkristian321@gmail.com>
2024-10-05 19:31:21 +0800

    added changes. implemented webtoken

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'secret_nato_bai';
+
 const loggingMiddleware = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-  };
-  
-  const authMiddleware = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+};
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
     }
+    req.userId = decoded.id;
     next();
-  };
-  
-  module.exports = { loggingMiddleware, authMiddleware };
+  });
+};
+
+module.exports = { loggingMiddleware, authMiddleware };
